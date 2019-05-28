d25d025e7340e187f90e9d692ba4b90a3101e78c
Amjad <amdabdulhameed@gmail.com>
2019-05-29 00:38:42 +0200

    implementing insertion sort

--- main.go
+++ main.go
@@ -35,11 +35,26 @@ func selectionSort(arr []int) []int {
 	return arr
 }
 
+func insertionSort(arr []int) []int {
+	for i := 1; i < len(arr); i++ {
+		key := arr[i]
+		prev := i - 1
+
+		for prev >= 0 && key < arr[prev] {
+			arr[prev+1] = arr[prev]
+			prev = prev - 1
+		}
+	}
+	return arr
+}
+
 func main() {
 	arr := []int{10, 1, 20, 4, 2, 9, 5, 7, 6} // your array
 	selection := selectionSort(arr)
 	bubble := bubbleSort(arr)
+	insertion := insertionSort(arr)
 
 	fmt.Printf("Selection sort: %d\n", selection)
 	fmt.Printf("Bubble sort: %d\n", bubble)
+	fmt.Printf("Insertion sort: %d\n", insertion)
 }
